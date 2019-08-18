1c99ea342325e12856e00a65ecb6b504bc80bd95
sirisha2002 <sirishapoorna@gmail.com>
2019-08-18 23:18:14 +0530

    class

---
+++ newclass.java
@@ -0,0 +1,23 @@
+package snapdealone;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class newclass {
+	
+public static void main(String[] args) {
+	System.setProperty("Webdriver.chrome.driver", "C:\\Users\\Janakiram\\eclipse-workspace\\Selenium1\\src\\july930final\\driver\\chromedriver.exe");
+	
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.snapdeal.com/");
+		WebElement s=driver.findElement(By.xpath("https://www.snapdeal.com/"));
+		s.sendKeys("HP laptop");
+		WebElement s1=driver.findElement(By.xpath("//span[@class='searchTextSpan']"));
+		s1.click();
+
+
+}
+
+}
