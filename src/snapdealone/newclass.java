8dd6c69e15ba1af3da868292099bf3560443a551
archanaGit17 <archana.sk95@gmail.com>
2019-08-18 15:37:27 +0530

    snap deal coded

--- src/snapdealone/newclass.java
+++ src/snapdealone/newclass.java
@@ -1,11 +1,21 @@
 package snapdealone;
 
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class newclass {
 	
-public static void main(String[] args) {
-		
+public static void main(String[] args) 
+{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\subbian\\eclipse-workspace\\Arch\\WindowsHandling\\driver\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.snapdeal.com/");
+	WebElement search=driver.findElement(By.xpath("//input[@id='inputValEnter']"));
+	search.sendKeys("iphone 7");
+	WebElement enter=driver.findElement(By.xpath("//button[@class='searchformButton col-xs-4 rippleGrey']"));
+	enter.click();	
 
 
 }
