6760f008485ea691de80bfc4959b2f63fd775148
sathishp3190 <sathishp3190@gmail.com>
2019-08-19 11:31:36 +0530

    Login Page Commited

--- src/snapdealone/newclass.java
+++ src/snapdealone/newclass.java
@@ -1,27 +1,23 @@
 package snapdealone;
 
-import java.util.Set;
-
-import org.apache.commons.exec.util.DebugUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.seleniumhq.jetty9.server.DebugListener;
-import org.seleniumhq.jetty9.server.handler.DebugHandler;
 
 public class newclass {
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chrome.driver", "H:\\Programs\\july930final\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shylaja\\eclipse-workspace\\july930final\\driver\\chromedriver76.exe");
 		
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.snapdeal.com/");
-		WebElement username=driver. findElement(By.id("og-url"));
-		username.sendKeys("7834893890");
+		driver.get("https://www.snapdeal.com/login");
+		WebElement username=driver. findElement(By.id("userName"));
+		username.sendKeys("7824027045");
 		System.out.println("Done");
-		WebElement username1=driver. findElement(By.id("mail"));
-		username1.sendKeys("sgs.vkl@gmail.com");
+		WebElement Continue = driver.findElement(By.id("checkUser"));
+		Continue.click();
+		
 			
 }
 }
